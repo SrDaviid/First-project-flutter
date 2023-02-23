b9c88c81394938bba047f6db3e9e89023722a0b5
David Frias <luis.david.08@hotmail.com>
2023-02-23 13:59:04 -0400

    eslint-save

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,9 +2,8 @@ import 'package:fl_components/screens/listview1_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,29 +12,26 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         elevation: 0,
       ),
-      body:ListView.separated(
-        itemBuilder: (context, index) => ListTile(
-          title: const Text('Nombre de ruta'),
-          leading: const Icon(Icons.account_circle_outlined),
-          onTap: () {
-            
-            final route = MaterialPageRoute(
-              builder:(context) => const Listview1Screen()
-              ,);
+      body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+                title: const Text('Nombre de ruta'),
+                leading: const Icon(Icons.account_circle_outlined),
+                onTap: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const Listview1Screen(),
+                  );
 
-            Navigator.pushNamed(context, "listview1");
+                  Navigator.pushNamed(context, "listview1");
 
-            // Con este metodo puedes volver hacia atras
-            // Navigator.push(context, route);
+                  // Con este metodo puedes volver hacia atras
+                  // Navigator.push(context, route);
 
-            // Con este metodo no puedes volver hacia atras
-            // Navigator.pushReplacement(context, route);
-
-          },
-        ), 
-        separatorBuilder: (_, __) => const Divider(), 
-        itemCount: 100
-        ),
-      );
+                  // Con este metodo no puedes volver hacia atras
+                  // Navigator.pushReplacement(context, route);
+                },
+              ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: 100),
+    );
   }
 }
