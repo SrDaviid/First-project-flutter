853445234a35ba798a772f96cbe5e2338f07cbc0
David Frias <luis.david.08@hotmail.com>
2023-03-02 10:01:05 -0400

    Comment old way to get routes and implement a new one

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -6,7 +6,7 @@ import 'package:fl_components/screens/screens.dart';
 class AppRoutes {
   static const initialRoute = 'home';
 
-  static final menuOption = <MenuOption>[
+  static final menuOptions = <MenuOption>[
     //TODO: borrar home
     MenuOption(
         route: 'home',
@@ -39,13 +39,24 @@ class AppRoutes {
         icon: Icons.credit_card_rounded),
   ];
 
-  static Map<String, Widget Function(BuildContext)> routes = {
-    'home': (BuildContext context) => const HomeScreen(),
-    'listview1': (BuildContext context) => const Listview1Screen(),
-    'listview2': (BuildContext context) => const Listview2Screen(),
-    'alert': (BuildContext context) => const AlertScreen(),
-    'card': (BuildContext context) => const CardScreen(),
-  };
+  static Map<String, Widget Function(BuildContext)> getAppRoutes() {
+    Map<String, Widget Function(BuildContext)> appRoutes = {};
+
+    for (final options in menuOptions) {
+      appRoutes
+          .addAll({options.route: (BuildContext context) => options.screen});
+    }
+
+    return appRoutes;
+  }
+
+  // static Map<String, Widget Function(BuildContext)> routes = {
+  //   'home': (BuildContext context) => const HomeScreen(),
+  //   'listview1': (BuildContext context) => const Listview1Screen(),
+  //   'listview2': (BuildContext context) => const Listview2Screen(),
+  //   'alert': (BuildContext context) => const AlertScreen(),
+  //   'card': (BuildContext context) => const CardScreen(),
+  // };
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
     return MaterialPageRoute(
