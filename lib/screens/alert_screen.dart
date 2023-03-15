55f474ab5cabfde622e2f0d6d813e83798d2fb4b
David Frias <luis.david.08@hotmail.com>
2023-03-15 09:10:55 -0400

    Add floating & elevated button in AlertScreen

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+          child: ElevatedButton(
+        // style: ElevatedButton.styleFrom(
+        //     backgroundColor: Colors.red,
+        //     shape: const StadiumBorder(),
+        //     elevation: 0),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+            "Mostrar alerta",
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        onPressed: () {},
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
     );
   }
