a91b8a8a91cd01955fff51a9ce2bba18581e0bae
srdaviid <luis.david.08@hotmail.com>
2023-03-28 17:12:55 -0400

    Create Avatar Screen

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -5,9 +5,26 @@ class AvatarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Stan Lee"),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 5),
+            child: CircleAvatar(
+              backgroundColor: Colors.indigo[900],
+              child: const Text('SL'),
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: CircleAvatar(
+          maxRadius: 120,
+          backgroundImage: NetworkImage(
+              "https://media.wired.com/photos/5be9d68a5d7c6a7b81d79e25/1:1/w_1800,h_1800,c_limit/StanLee-610719480.jpg"),
+        ),
       ),
     );
   }
