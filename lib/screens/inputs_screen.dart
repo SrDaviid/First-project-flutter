21539b0ac41fe9859752c6258b3da11fcbb4ffcb
SrDaviid <luis.david.08@hotmail.com>
2023-04-23 21:44:12 -0400

    adding global key and CustomInputField,

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,50 +1,72 @@
 import 'package:flutter/material.dart';
+import '../widgets/widget.dart';
 
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+    final Map<String, String> formValues = {
+      'first_name': 'David',
+      'second_name': 'Frias',
+      'email': 'david@hotmail.com',
+      'password': '123456',
+      'role': 'Admin',
+    };
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Hello World'),
+        title: const Text('Inputs and forms'),
       ),
       //Widget que nos permite hacer scroll
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(children: [
-            TextFormField(
-              //This property allow me to auto direct the form with the keyword
-              autofocus: false,
-              //This property put a initial text on the form
-              initialValue: '',
-              //This property allow me to start a word in uppercase
-              textCapitalization: TextCapitalization.words,
-              onChanged: (value) {
-                print('value: $value');
-              },
-              validator: (value) {
-                if (value == null) return 'This field is required';
-                return value.length < 3 ? 'The minimun is 3' : null;
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(
-                  hintText: 'Username',
-                  labelText: 'Name',
-                  helperText: 'Only text',
-                  // counterText: '3 words left'
-                  suffixIcon: Icon(
-                    Icons.group_outlined,
-                    color: Colors.indigo,
-                  ),
-                  // prefixIcon: Icon(
-                  //   Icons.verified_user_sharp,
-                  // ),
-                  icon: Icon(Icons.assignment_ind_outlined)),
-            )
-          ]),
+          child: Form(
+            key: myFormKey,
+            child: Column(children: [
+              const CustomInputField(
+                labelText: 'Name',
+                hintText: 'Username',
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+              const CustomInputField(
+                labelText: 'Last Name',
+                hintText: 'Subname',
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+              const CustomInputField(
+                labelText: 'User mail',
+                hintText: 'Email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+              const CustomInputField(
+                labelText: 'Password',
+                hintText: 'User password',
+                obscureText: true,
+              ),
+              const Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+              ElevatedButton(
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+                    if (!myFormKey.currentState!.validate()) {
+                      print('Form not valid');
+                      return;
+                    }
+                    //* Imprimir valores del formulario
+                    print(formValues);
+                  },
+                  child: const SizedBox(
+                      width: double.infinity,
+                      child: Text(
+                        'Save',
+                        textAlign: TextAlign.center,
+                      )))
+            ]),
+          ),
         ),
       ),
     );
